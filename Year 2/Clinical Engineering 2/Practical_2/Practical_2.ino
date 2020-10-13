f6c1225b12b3b484b1b9e5c771507d138a697d4c
Jonathan <jj.holt@live.co.uk>
2020-10-13 01:14:01 +0100

    initial upload

---
+++ Year 2/Clinical Engineering 2/Practical_2/Practical_2.ino
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+static inline void initADC0(void) {
+  ADMUX |= (1 << REFS0);
+  ADCSRA |= (1 << ADPS1) | (1 << ADPS0);
+  ADCSRA |= (1 << ADIE);
+  ADCSRA |= (1 << ADEN);
+  ADCSRA |= (1 << ADSC);
+}
+
+void setup() {
+  DDRB |= 0b00100000; //initiate IO ports
+}
+
+ISR (ADC_vect) {
+  if(ADC > 512) {
+  PORTB = 0b00100000;
+  } else {PORTB = 0b00000000;}
+  ADCSRA |= (1 << ADSC);
+}
+
+void loop() {
+  Serial.begin(9600);
+  initADC0();
+  sei();
+  
+  while(1){
+    PORTB ^= 0b00100000;
+    Serial.println(ADC);
+
+  }
+}
