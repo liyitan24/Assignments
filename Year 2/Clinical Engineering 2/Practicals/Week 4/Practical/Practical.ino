6241d4d2d99dea9c2db5c5bce2d6b21fad76ea6f
Jonathan Holt <jjholt@Jonathans-MBP.broadband>
2020-11-09 15:29:07 +0000

    Added Timer 1 interrupt

--- Year 2/Clinical Engineering 2/Practicals/Week 4/Practical/Practical.ino
+++ Year 2/Clinical Engineering 2/Practicals/Week 4/Practical/Practical.ino
@@ -1,17 +1,17 @@
 #include <avr/io.h>
 
 int cyclesPerSample{79}; //200 Hz; 2x maximum frequency of ECG
-//long int cyclesPerSample{80000}; //200 Hz; 2x maximum frequency of ECG
 int _adc{0};
-int doPrint{0};
+int shouldPrint{0}; //controlled printing. requires truthy value for print
+
 static inline void initSampler(void) {
     TCCR0B = 0b00000101; //prescaler: 1024
-//    TCCR0B = 0b00000001; //No scaling
     TIMSK0 |= (1 << TOIE1); // overflow interrupt enable 
     TCNT0 = 2^8-cyclesPerSample;
 }
 static inline void countToOneSec(void) {
     TCCR1B = 0b00000101; //prescaler: 1024
+    TIMSK1 |= (1 << TOIE1); // overflow interrupt enable
     TCNT1=2^16-15625; //cycles to 1 second
 }
 
@@ -26,21 +26,20 @@ static inline void initADC(void) {
     ADCSRA |= (1 << ADEN); // ADC Enable
     ADCSRA &= ~(1 << ADATE); //ADC Autotrigger - Make sure it's off, otherwise it triggers without interrupts.
 }
-
 // ------ Interrupt Service Routine ------ //
 ISR(TIMER0_OVF_vect) {
     ADCSRA |= (1 << ADSC); //Start Conversion
     TCNT0=2^8-cyclesPerSample;//Reset timer
 }
+ISR(TIMER1_OVF_vect) {
+    TIMSK0 &= ~(1 << TOIE1); // overflow interrupt disable
+    
+}
 
 ISR(ADC_vect) {
     _adc = ADC;
-    doPrint=1;
-    #if 0 //Let me know something is happening
-        if(ADC > 0) {
-            PORTB = 0b00100000;
-        } else {PORTB = 0b00000000;}
-    #endif
+    // _adc = TCNT1 //print time to check
+    shouldPrint=1;
 }
 
 int main(void) {
@@ -52,12 +51,12 @@ int main(void) {
     initSampler();
     interrupts();
   
-     while(TCNT1 > 0) {
-         Serial.flush();
-         if(doPrint){
-             Serial.println(_adc);
-             doPrint=0;
-         }
+     while(1) {
+        Serial.flush();
+        if(shouldPrint){
+            Serial.println(_adc);
+            shouldPrint = 0;
+        }
      }
     return (0);
 }
