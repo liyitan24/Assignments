1c1bf85a6d3fd1942f60c64a4afdc8254ae52a6e
Jonathan Holt <jjholt@Jonathans-MBP.broadband>
2020-11-09 13:48:22 +0000

    timing tested

--- Year 2/Clinical Engineering 2/Week 4/Practical/Practical.ino
+++ Year 2/Clinical Engineering 2/Week 4/Practical/Practical.ino
@@ -1,18 +1,18 @@
 #include <avr/io.h>
 
-int cyclesBeforeOneSec{15625};
-int cyclesBeforeOverflow{1000}; //Max heart rate is about 200ish bpm, so 3hz-ish
+int cyclesPerSample{79}; //200 Hz; 2x maximum frequency of ECG
+//long int cyclesPerSample{80000}; //200 Hz; 2x maximum frequency of ECG
 int _adc{0};
 int doPrint{0};
-static inline void initTimer(void) {
+static inline void initSampler(void) {
     TCCR0B = 0b00000101; //prescaler: 1024
-    TCNT0 = 2^8-cyclesBeforeOverflow;
+//    TCCR0B = 0b00000001; //No scaling
     TIMSK0 |= (1 << TOIE1); // overflow interrupt enable 
+    TCNT0 = 2^8-cyclesPerSample;
 }
-static inline int countToOneSec(void) {
-    TCNT1=2^16-cyclesBeforeOneSec;
+static inline void countToOneSec(void) {
     TCCR1B = 0b00000101; //prescaler: 1024
-    return TCNT1;
+    TCNT1=2^16-15625; //cycles to 1 second
 }
 
 static inline void initIOPorts(void) {
@@ -20,7 +20,7 @@ static inline void initIOPorts(void) {
 }
 
 static inline void initADC(void) {
-    ADMUX |= (1 << REFS0);
+    ADMUX  |= (1 << REFS0);
     ADCSRA |= (1 << ADPS1) | (1 << ADPS0); //ADC Prescaler: 8
     ADCSRA |= (1 << ADIE); // ADC Interrupt enabled
     ADCSRA |= (1 << ADEN); // ADC Enable
@@ -30,34 +30,34 @@ static inline void initADC(void) {
 // ------ Interrupt Service Routine ------ //
 ISR(TIMER0_OVF_vect) {
     ADCSRA |= (1 << ADSC); //Start Conversion
-    TCNT1=2^8-cyclesBeforeOverflow;//Reset timer
+    TCNT0=2^8-cyclesPerSample;//Reset timer
 }
 
 ISR(ADC_vect) {
     _adc = ADC;
+    doPrint=1;
     #if 0 //Let me know something is happening
         if(ADC > 0) {
             PORTB = 0b00100000;
         } else {PORTB = 0b00000000;}
     #endif
-    doPrint=1;
 }
 
 int main(void) {
     Serial.begin(250000);
     noInterrupts();           
     initIOPorts();
-    countToOneSec();
-    initTimer();
     initADC();
+    countToOneSec();
+    initSampler();
     interrupts();
   
-    while(countToOneSec() > 1) {
-        Serial.flush();
-        if(doPrint==1){
-            Serial.println(_adc);
-            doPrint=0;
-        }
-    }
+     while(TCNT1 > 0) {
+         Serial.flush();
+         if(doPrint){
+             Serial.println(_adc);
+             doPrint=0;
+         }
+     }
     return (0);
 }
